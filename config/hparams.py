5bd5feb7801635c8deb29068adc39c0bb48d9b31
stephano15671567 <stephano.almendra1567@gmail.com>
2026-03-10 02:32:54 -0300

    chore: configuracion de rutas y parametros para 30 FPS

--- config/hparams.py
+++ config/hparams.py
@@ -1,3 +1,12 @@
+﻿# Hiperparametros del Modelo
+INPUT_DIM = 225
+EMBED_DIM = 256
+NHEAD = 8
+NUM_LAYERS = 4
+DROPOUT = 0.1
+FRAME_RATE = 30
 
-# RUTA REAL SEGUN VS CODE
+# RUTAS DE SISTEMA (Configuracion para el Xeon)
 VIDEO_ROOT = 'D:/ASL2.0/Youtube-ASL/data'
+CSV_PATH = 'data/corpus_final.csv'
+H5_OUTPUT = 'data/dataset_30fps.h5'
